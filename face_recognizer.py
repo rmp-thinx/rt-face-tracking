f4810705fadef627acd891d9e65830be7e41993e
Sebastian Medina <srmedinac@unal.edu.co>
2019-08-14 11:54:40 -0500

    Added facial landmark recognition + an already trained predictor

--- face_recognizer.py
+++ face_recognizer.py
@@ -3,7 +3,8 @@ import cv2  # OpenCV
 import numpy as np  # Handling data
 import time
 import os, sys
-
+from imutils import face_utils
+import dlib
 from pubnub.callbacks import SubscribeCallback
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
@@ -187,13 +188,15 @@ known_face_names = [
     "rodri",
     "sebas"
 ]
-
+p = "shape_predictor_68_face_landmarks.dat"
 # Initialize some variables
 face_locations = []
 face_encodings = []
 face_names = []
+face_landmarks_list = []
 process_this_frame = True
-
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(p)
 while (True):
 
     video_capture = cv2.VideoCapture(0)
@@ -201,17 +204,17 @@ while (True):
     ret, frame = video_capture.read()
 
     # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.20, fy=0.20)
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
-
+    rects = detector(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
     # Only process every other frame of video to save time
     if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
+        face_landmarks_list = face_recognition.face_landmarks(rgb_small_frame, face_locations)
         face_names = []
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
@@ -238,6 +241,13 @@ while (True):
                 # Alert()
 
     process_this_frame = not process_this_frame
+
+    for (i, rect) in enumerate(rects):
+        shape = predictor(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), rect)
+        shape = face_utils.shape_to_np(shape)
+
+        for (x,y) in shape:
+            cv2.circle(frame, (x,y), 2, (0,255,0), -1)
 
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
